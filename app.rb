84ad95fcd2f9d4d76030fc0310c93e38248c030a
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-19 22:52:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
-require_relative 'config/environment'
+class ApplicationController < Sinatra::Base
 
-class App < Sinatra::Base
+  configure do
+  	set :views, "app/views"
+  	set :public_dir, "public"
+  end
 
 
+  get "/" do
+    erb :index
+  end
+  post '/' do
+  	@the_lane = get_lane_confirmation
+  	@the_user = [:user]
+  	return erb :results
+  	
+  end
 end
---
+++ models/models.rb
@@ -0,0 +1,6 @@
+def get_lane_confirmation
+  lane_confirmation = ["#1", "#2", "#3", "#4"]
+return lane_confirmation.sample
+end 
+
+puts get_lane_confirmation
